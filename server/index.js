069167372cfa2a05975d5d34369b6bd7fc464039
Samuel117 <samuel.padilla45@gmail.com>
2023-06-12 20:49:05 -0600

    reqyuere

--- server/index.js
+++ server/index.js
@@ -1,13 +1,7 @@
-import express from "express"
-import mysql from "mysql"
-import cors from "cors"
-//import path from "path"
-import { fileURLToPath } from "url"
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-const path = require("path");
+const express = require("express");
+const mysql = require('mysql');
+const cors = require('cors');
+const path = require('path');
 
 const app = express()
 
